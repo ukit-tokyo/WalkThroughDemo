d04f32a0a7476c2a6faf0ee43f9eeda12eecd224
ukit-tokyo <t.yuki@ukit.tokyo>
2023-01-25 13:16:38 +0900

    Create project

---
+++ WalkThroughDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WalkThroughDemo
+//
+//  Created by Taichi Yuki on 2023/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.backgroundColor = .red
+  }
+}
+
